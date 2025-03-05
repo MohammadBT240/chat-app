b04d82b8875e01b796b2296d251ca22614ab6938
Mohammad Tukur <mohammadtukur@Mohammads-MacBook-Pro.local>
2025-03-05 10:49:02 +0100

    Completed Settings Page

---
+++ frontend/src/store/useThemeStore.js
@@ -0,0 +1,8 @@
+import { create } from "zustand";
+
+export const useThemeStore = create((set) => ({
+  theme: localStorage.getItem("theme") || "coffee",
+  setTheme: (theme) => {
+    set({ theme });
+  },
+}));
